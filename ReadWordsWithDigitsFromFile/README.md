cccd72b5e2e28f7e2a1abb33c929c0a6c617cde7
Аббас Гусенов <gusenov@live.ru>
2018-05-11 18:38:24 +0600

    Чтение текста из файла и вывод на экран слов с цифрами.

---
+++ ReadWordsWithDigitsFromFile/README.md
@@ -0,0 +1,12 @@
+# Test
+
+![INPUT](screenshots/input.png)
+
+![OUTPUT](screenshots/output.png)
+
+# Links
+
+- [c# - Reading a text file word by word - Stack Overflow](https://stackoverflow.com/a/9740611/2289640)
+- [c# - Check if a string has at least one number in it using LINQ - Stack Overflow](https://stackoverflow.com/a/1540633/2289640)
+- [Read txt files (in unicode and utf8) by means of C# - Stack Overflow](https://stackoverflow.com/questions/18871603/read-txt-files-in-unicode-and-utf8-by-means-of-c-sharp)
+- [Ответы@Mail.Ru: Какие еще есть слова с цифрами?](https://otvet.mail.ru/question/48777578)
---
+++ ReadWordsWithDigitsFromFile/ReadWordsWithDigitsFromFile/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadWordsWithDigitsFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа № 3");
+            Console.WriteLine("Название: Чтение текста из файла и вывод на экран слов с цифрами.");
+            Console.WriteLine("ФИО студента: ");
+            Console.WriteLine("Группа студента: ");
+            Console.WriteLine("Дата выполнения: 17 апреля 2018 года");
+            Console.WriteLine();
+            
+            String[] words;
+
+            using (TextReader reader = File.OpenText("input.txt")) {
+                words = reader.ReadToEnd().Split(new char[] { ' ', '\n' });
+            }
+
+            Console.Out.WriteLine("Вывод на экран слов с цифрами:");
+            foreach (var word in words)
+            {
+                if (word.Any(char.IsDigit))
+                {
+                    Console.Out.WriteLine(word);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
