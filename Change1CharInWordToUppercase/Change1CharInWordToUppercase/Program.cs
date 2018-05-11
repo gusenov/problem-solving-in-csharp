a48bfed9af6d5e11c8074aa25d6c775353465f7d
Аббас Гусенов <gusenov@live.ru>
2018-05-11 18:39:00 +0600

    Замена во введенной с клавиатуры строке первых символов слов на символы в верхнем регистре.

---
+++ Change1CharInWordToUppercase/Change1CharInWordToUppercase/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Change1CharInWordToUppercase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа № 4");
+            Console.WriteLine("Название: Замена во введенной с клавиатуры строке\n"
+                + "\tпервых символов слов на символы в верхнем регистре.");
+            Console.WriteLine("ФИО студента: ");
+            Console.WriteLine("Группа студента: ");
+            Console.WriteLine("Дата выполнения: 17 апреля 2018 года");
+            Console.WriteLine();
+
+            Console.Out.WriteLine("Введите с клавиатуры строку -> ");
+            String inputLine = Console.ReadLine();
+            
+            String[] words = inputLine.Split(' ');
+            
+            StringBuilder outputResult = new StringBuilder();
+            foreach (var word in words)
+            {
+                if (word.Length > 0)
+                {
+                    outputResult.Append(char.ToUpper(word[0]));
+                    if (word.Length > 1)
+                    {
+                        outputResult.Append(word.Substring(1));
+                    }
+                }
+                outputResult.Append(" ");
+            }
+
+            Console.Out.WriteLine("Результирующая строка:\n{0}", outputResult.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Change1CharInWordToUppercase/README.md
@@ -0,0 +1,17 @@
+# Test Cases
+
+## № 1
+
+![Съешь ещё этих мягких французских булок да выпей чаю](screenshots/test1.png)
+
+## № 2
+
+![Широкая электрификация южных губерний даст мощный толчок подъёму сельского хозяйства](screenshots/test2.png)
+
+## № 3
+
+![The quick brown fox jumps over the lazy dog](screenshots/test3.png)
+
+# Links
+
+- [C# Console.ReadLine Example - Dot Net Perls](https://www.dotnetperls.com/console-readline)
